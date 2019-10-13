25da1be20a982f88c529208c242eb82a29a6dec3
Maharavotahirinirinaolivier <mamadotahiry@gmail.com>
2019-10-13 13:08:23 +0200

    'tah'

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './App.css';
+import Test from "./composant/test";
+
+
+class App extends Component{
+
+ 
+
+  render(){
+  return (
+
+     <div>
+                  
+                        
+          <Test/>                  
+                
+   
+    </div>
+
+)
+}
+}
+
+export default App;
